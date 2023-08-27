2400cb6f1a43df3dca3676333ec2ceac06334f9a
Elena <franchini.elena97@gmail.com>
2023-08-27 10:35:42 +0200

    Add cubes border color, build shapes of tetrominoes, build figure method

--- game/game.js
+++ game/game.js
@@ -5,18 +5,15 @@ const ctx = canvas.getContext("2d");
 
 
 function run() {
-    var size = 60;
+    const size = 60;
     var cubes = new Array();
-    var x_coord = new Array(0,60,120,180,240,300,360,420,480,540);
-    var init_coord_x = x_coord[Math.floor(Math.random() * x_coord.length)]
-    const init_coord_y = 0;
+    const x_coord = new Array(0,60,120,180,240,300,360,420,480,540);
     const tetrominoes = new Array('I', 'O', 'T', 'S', 'Z', 'J');
     const colors = new Array('yellow', 'orange', 'red', 'green', 'blue', 'aqua', 'purple')
-    let color = Math.floor(Math.random() * colors.length);
-    // create first cube objest
-    var cube = create_cube(init_coord_x,init_coord_y, size, colors[color]);
 
-    I_shape(cube, cubes);
+    // continue to call
+    figures(tetrominoes, size, x_coord, colors, cubes);
+    // I_shape(cubes, x_coord);
     // setInterval(() => {
     //     cube = move(cube);
     // }, 1000);
@@ -30,44 +27,66 @@ tetromino -> random figure shape to create
 cube -> cube object 
 cubes -> array of cube objects
 */
-function figures(tetromino, cube, cubes) {
-    cubes.push(cube);
-    x = cube.x;
-    y = cube.y;
+function figures(tetrominoes, size, x_coord, colors, cubes) {
+    let color = colors[Math.floor(Math.random() * colors.length)];
+
+    init_coord_x = x_coord[Math.floor(Math.random() * x_coord.length)];
+    I_shape(init_coord_x, size, color, cubes);
+
+
 }
 
-function I_shape(cube,cubes) {
-    let new_cube = {};
-    if (cube.x + 4*60 <= 540) {
-        for (let i=60; i <= 60*3; i += 60) {
-            new_cube = create_cube(cube.x + i, cube.y, cube.size, cube.color);
-            cubes.push(new_cube);
+function I_shape(x, size, color, cubes) {
+    let new_cube = {}
+    for (let i = 0; i <= 60*3; i += 60) {
+        if (x + 3*60 <= 540) {
+            new_cube = create_cube(x + i, 0, size, color);
+        } else {
+            new_cube = create_cube(x - i, 0, size, color);
         }
-    } else {
-        for (let i=-60; i >= -60*3; i -= 60) {
-            new_cube = create_cube(cube.x + i, cube.y, cube.size, cube.color);
-            cubes.push(new_cube);
+        cubes.push(new_cube);
+    }
+}
+
+function O_shape(x, size, color, cubes) {
+    let new_cube = {};
+    for (let i = 0; i <= 60; i += 60) {
+        for (let j = 0; j <= 60; j += 60) {
+            if (x + 60 <= 540) {
+                new_cube = create_cube(x + i, j, size, color);
+            } else {
+                new_cube = create_cube(x - i, j, size, color);
+            }
+            cubes.push(new_cube); 
         }
     }
 }
 
-function O_shape(cube) {
+function T_shape(x, size, color, cubes) {
+    let new_cube = {};
+    for (let i = 0; i <= 60*2; i += 60) {
+        if (x + 60*2 <= 540) {
+            new_cube = create_cube(x + i, 0, size, color);
+            cubes.push(new_cube); 
+            new_cube = create_cube(x + 60, i, size, color);
+        } else {
+            new_cube = create_cube(x - i, 0, size, color);
+            cubes.push(new_cube); 
+            new_cube = create_cube(x - 60, i, size, color);
+        }
+        cubes.push(new_cube); 
+    }
+}
+
+function S_shape(x, size, color, cubes) {
 
 }
 
-function T_shape(cube) {
+function Z_shape(x, size, color, cubes) {
 
 }
 
-function S_shape(cube) {
-
-}
-
-function Z_shape(cube) {
-
-}
-
-function J_shape(cube) {
+function J_shape(x, size, color, cubes) {
 
 }
 
@@ -79,8 +98,16 @@ function move(cube_obj) {
     size = cube_obj.size;
     clear(cube_obj);
     ctx.beginPath();
+    // ctx.fillStyle = color;
+    // ctx.fillRect(old_x,old_y + 60, size, size);
     ctx.fillStyle = color;
-    ctx.fillRect(old_x,old_y + 60, size, size);
+    ctx.strokeStyle = 'black';
+    var fillRect = false;
+    ctx.rect(old_x, old_y + 60, size, size);
+    if (fillRect) {
+    ctx.fill();
+    }
+    ctx.stroke();
     cube_obj = {x:old_x, y:old_y+ 60, size:size, color:color};
     ctx.stroke();
     return cube_obj;
@@ -112,8 +139,15 @@ function create_cube(x,y,size, color) {
     // build cube object
     const cube = {x:x, y:y, size:size, color:color};
     ctx.beginPath();
+    // ctx.fillStyle = color;
+    // ctx.fillRect(x,y, size, size);
     ctx.fillStyle = color;
-    ctx.fillRect(x,y, size, size);
+    ctx.strokeStyle = 'black';
+    var fillRect = true;
+    ctx.rect(x, y, size, size);
+    if (fillRect) {
+    ctx.fill();
+    }
     ctx.stroke();
     return cube;
 }
