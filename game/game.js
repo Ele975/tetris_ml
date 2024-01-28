7445eb0d74a82499406a889615e51940d1a00446
Ele975 <55561956+Ele975@users.noreply.github.com>
2024-01-29 00:47:11 +0100

    implement move left, right and delete rows

--- game/game.js
+++ game/game.js
@@ -3,29 +3,33 @@ const ctx = canvas.getContext("2d");
 
 // tile size
 const size = 60;
-const colors = new Array('yellow', 'orange', 'red', 'green', 'blue', 'aqua', 'purple')
+const colors = new Array('gold', 'orange', 'red', 'green', 'blue', 'aqua', 'purple')
 // call function for given shape
 const tetrominoes = { "I": I_shape, "O": O_shape, "T": T_shape, "S": S_shape, "Z": Z_shape, "L": L_shape, "J": J_shape };
 // const x_coords = new Array(0, 60, 120, 180, 240, 300, 360, 420, 480, 540);
 
-// array of all cubes
+// array of all cubes. Cubes of current figure are appended at the end when it stops
 var cubes = new Array();
 var cube_id = 0;
 // array of cubes of current figure (moving one)
 var current_cubes = null;
-var current_state = 'move';
+var current_state = 'moving';
 
 function init() {
     // create init fig
     create_fig();
     // intervalId = setInterval(() => run(), 1000);
-    intervalId = setInterval(() => run(), 100);
+    intervalId = setInterval(() => run(), 500);
 }
 
 function run() {
+    // exit if game over -> no creation of other figures and stop calling run method
+    // console.log(cubes)
+    check_full_rows()
     exit = false;
-    bottom_reached = check_move_down();
-    if (bottom_reached) {
+    occupied = check_move("slide_down");
+    if (occupied) {
+        current_state = "still"
         for (let i = 0; i < current_cubes.length; i++) {
             if (current_cubes[i].y <= 0) {
                 console.log('GAME OVER');
@@ -34,8 +38,13 @@ function run() {
             }
         }
         if (!exit) {
+            // cubes
+            cubes = cubes.concat(current_cubes)
             create_fig();
         }
+    } else {
+        // if new coordinates are occupied don't move, otherwise move down
+        update_pos(current_cubes, 'slide_down')
     }
 }
 
@@ -55,6 +64,7 @@ function create_fig() {
     const rnd_key = keysArray[Math.floor(Math.random() * keysArray.length)];
     let val = tetrominoes[rnd_key];
     val(init_coord_x, color)
+    current_state = "moving"
 }
 
 /*
@@ -63,44 +73,70 @@ Param:
 figures -> array of figure objects
 */
 
-function check_move_down() {
-    let bottom_cubes = new Array();
-    let lower_y = -100
-    // find bottom y value of tiles in current figure
-    for (let i = 0; i < current_cubes.length; i++) {
-        if (current_cubes[i].y >= lower_y) {
-            lower_y = current_cubes[i].y
-        }
+function check_move(movement) {
+    switch (movement) {
+        case 'slide_down':
+            return check_slide_down();
+        case "move_left":
+            return check_move_left();
+        case "move_right":
+            return check_move_right();
     }
-    // extract all tiles in figure in bottom position
-    for (let i = 0; i < current_cubes.length; i++) {
-        if (current_cubes[i].y == lower_y) {
-            bottom_cubes.push(current_cubes[i])
-        }
-    }
+}
 
-    let occupied = false;
-    for (let i = 0; i < bottom_cubes.length; i++) {
-        let new_y = bottom_cubes[i].y + 60;
-        let coord = [bottom_cubes[i].x, new_y];
-        // check if new tile is already occupied by another block
+function check_slide_down() {
+    occupied = false
+    for (let i = 0; i < current_cubes.length; i++) {
+        if (current_cubes[i].y + 60 >= 780) {
+            occupied = true;
+            break;
+        }
         for (let j = 0; j < cubes.length; j++) {
-            if (cubes[j].x == coord[0] && cubes[j].y == coord[1]) {
+            if (current_cubes[i].x == cubes[j].x && current_cubes[i].y + 60 == cubes[j].y) {
                 occupied = true;
                 break;
             }
         }
-        // check if bottom game box reached
-        if ((!occupied) && new_y >= 780) {
+    }
+    return occupied;
+}
+
+function check_move_left() {
+    occupied = false
+    // check intersection with left border
+    for (let i = 0; i < current_cubes.length; i++) {
+        if (current_cubes[i].x == 0) {
             occupied = true;
             break;
         }
+        // check intersection with other figures
+        for (let j = 0; j < cubes.length; j++) {
+            if (current_cubes[i].x - 60 == cubes[j].x && current_cubes[i].y == cubes[j].y) {
+                occupied = true;
+                break;
+            }
+        }
     }
-    // if new coordinates are occupied don't move, otherwise move down
-    if (!occupied) {
-        update_pos(current_cubes, 'current_fig')
-    } 
-    return occupied
+    return occupied;
+}
+
+function check_move_right() {
+    occupied = false
+    // check intersection with right border
+    for (let i = 0; i < current_cubes.length; i++) {
+        if (current_cubes[i].x == 540) {
+            occupied = true;
+            break;
+        }
+        // check intersection with other figures
+        for (let j = 0; j < cubes.length; j++) {
+            if (current_cubes[i].x + 60 == cubes[j].x && current_cubes[i].y == cubes[j].y) {
+                occupied = true;
+                break;
+            }
+        }
+    }
+    return occupied;  
 }
 
 /*
@@ -109,39 +145,28 @@ Param:
 cubes -> cubes which have to move
 type -> type of cubes
 */
-function update_pos(current_cubes, type) {
-    if (type == 'current_fig') {
-        for (let k = 0; k < current_cubes.length; k++) {
-            // clear previous figure positions
-            ctx.clearRect(current_cubes[k].x, current_cubes[k].y, current_cubes[k].size, current_cubes[k].size);
-            // update y positions in current_cubes array
-            current_cubes[k].y += 60
-            // update y positions in cubes array
-            for (let i = 0; i < cubes.length; i++) {
-                if (current_cubes[k].x == cubes[i].x && current_cubes[k].y - 60 == cubes[i].y) {
-                    // occupied_tiles[i][1] += 60;
-                    break;
-                }
-            }
-        }
-        for (let k = 0; k < current_cubes.length; k++) {
-            // draw new figure positions
-            create_cube(current_cubes[k].x, current_cubes[k].y, current_cubes[k].size, current_cubes[k].color, current_cubes[k].id)
-        }
+function update_pos(cubes_type, operation_type) {
+    if (operation_type == 'slide_down' || operation_type == 'del_row') {
+        change_x = 0
+        change_y = 60
+    } else if (operation_type == "move_left") {
+        change_x = -60
+        change_y = 0
+    } else if (operation_type == "move_right") {
+        change_x = 60
+        change_y = 0
     }
-
-}
-
-function rotate_left() {
-
-}
-
-function rotate_right() {
-
-}
-
-function clear(cube_obj) {
-    ctx.clearRect(cube_obj.x, cube_obj.y, cube_obj.size, cube_obj.size);
+    for (let k = 0; k < cubes_type.length; k++) {
+        // clear previous figure positions
+        ctx.clearRect(cubes_type[k].x, cubes_type[k].y, cubes_type[k].size, cubes_type[k].size);
+        // update y positions in current_cubes array
+        cubes_type[k].x += change_x
+        cubes_type[k].y += change_y
+    }
+    for (let k = 0; k < cubes_type.length; k++) {
+        // draw new figure positions
+        create_cube(cubes_type[k].x, cubes_type[k].y, cubes_type[k].size, cubes_type[k].color, cubes_type[k].id)
+    }
 }
 
 
@@ -182,7 +207,7 @@ function I_shape(x, color) {
 
     for (i = x - 60; i <= x + 120; i += 60) {
         new_cube = create_cube(i,-60,size,color);
-        cubes.push(new_cube);
+        // cubes.push(new_cube);
         temp_cubes.push(new_cube)
     }  
     current_cubes = temp_cubes;  
@@ -194,7 +219,7 @@ function O_shape(x, color) {
     for (let i = x; i <= x + 60; i += 60) {
         for (let j = -60; j <= 0; j += 60) {
             new_cube = create_cube(i, j, size, color);
-            cubes.push(new_cube);
+            // cubes.push(new_cube);
             temp_cubes.push(new_cube)
         }
     }
@@ -207,11 +232,11 @@ function T_shape(x, color) {
 
     for (let i = x - 60; i <= x + 60; i+= 60) {
         new_cube = create_cube(i, -120, size, color);
-        cubes.push(new_cube);
+        // cubes.push(new_cube);
         temp_cubes.push(new_cube)
     }
     new_cube = create_cube(x, -60, size, color);
-    cubes.push(new_cube);
+    // cubes.push(new_cube);
     temp_cubes.push(new_cube);
     current_cubes = temp_cubes;
 }
@@ -221,12 +246,12 @@ function S_shape(x, color) {
     let temp_cubes = []
     for (let i = x; i <= x + 60; i += 60) {
     new_cube = create_cube(i, -120, size, color);
-    cubes.push(new_cube);
+    // cubes.push(new_cube);
     temp_cubes.push(new_cube)
     }
     for (let i = x - 60; i <= x; i += 60) {
     new_cube = create_cube(i, -60, size, color);
-    cubes.push(new_cube);
+    // cubes.push(new_cube);
     temp_cubes.push(new_cube)
     }
     current_cubes = temp_cubes;
@@ -237,13 +262,13 @@ function Z_shape(x, color) {
     let temp_cubes = []
     for (let i = x - 60; i <= x; i += 60) {
         new_cube = create_cube(i, -120, size, color);
-        cubes.push(new_cube);
+        // cubes.push(new_cube);
         temp_cubes.push(new_cube)
     }
 
     for (let i = x; i <= x + 60; i += 60) {
         new_cube = create_cube(i, -60, size, color);
-        cubes.push(new_cube);
+        // cubes.push(new_cube);
         temp_cubes.push(new_cube)
     }
     current_cubes = temp_cubes;
@@ -254,11 +279,11 @@ function L_shape(x, color) {
     let temp_cubes = []
     for (let i = -180; i <= -60; i += 60) {
         new_cube = create_cube(x, i, size, color);
-        cubes.push(new_cube);
+        // cubes.push(new_cube);
         temp_cubes.push(new_cube)
     }
     new_cube = create_cube(x + 60, -60, size, color);
-    cubes.push(new_cube);
+    // cubes.push(new_cube);
     temp_cubes.push(new_cube)
     current_cubes = temp_cubes;
 }
@@ -268,13 +293,90 @@ function J_shape(x, color) {
     let temp_cubes = []
     for (let i = -180; i <= -60; i += 60) {
         new_cube = create_cube(x, i, size, color);
-        cubes.push(new_cube);
+        // cubes.push(new_cube);
         temp_cubes.push(new_cube)
     }
     new_cube = create_cube(x - 60, -60, size, color);
-    cubes.push(new_cube);
+    // cubes.push(new_cube);
     temp_cubes.push(new_cube)
     current_cubes = temp_cubes;
+}
+
+// listen for key event to rotate figures
+document.addEventListener('keydown', function(event) {
+    if (current_state == "moving") {
+        if (event.key == "ArrowLeft") {
+            console.log('left')
+            occupied = check_move_left()
+            if (!occupied) {
+                update_pos(current_cubes, "move_left")
+            }
+        } else if (event.key == "ArrowRight") {
+            console.log('right')
+            occupied = check_move_right()
+            if (!occupied) {
+                update_pos(current_cubes, "move_right")
+            }
+        } else if (event.key == "ArrowUp") {
+            console.log('up')
+            rotate_left()
+        } else if (event.key == "ArrowDown") {
+            console.log('down')
+            rotate_right()
+        }
+    }
+});
+
+function rotate_left() {
+    console.log('Called left')
+}
+
+function rotate_right() {
+    console.log('Called right')
+}
+
+function clear(cube_obj) {
+    ctx.clearRect(cube_obj.x, cube_obj.y, cube_obj.size, cube_obj.size);
+}
+
+function check_full_rows() {
+    // 10
+    // store y coordinates as key and amount of tiles in that row as value
+    let row_count = {}
+    for (let i = 0; i < cubes.length; i++) {
+        if (String(cubes[i].y) in row_count) {
+            row_count[String(cubes[i].y)] += 1
+        } else {
+            row_count[String(cubes[i].y)] = 1
+        }
+    }
+    console.log(row_count)
+
+    // find full rows (10 tiles)
+    for (let key in row_count) {
+        if (row_count[key] == 10) {
+            delete_full_row(parseInt(key))
+        }
+    }
+}
+
+function delete_full_row(y_coord) {
+    console.log(y_coord)
+    // delete all tiles in the given y coordinate
+    let cubes_temp = []
+    for (let i = 0; i < cubes.length; i++) {
+        if (cubes[i].y == y_coord) {
+            ctx.clearRect(cubes[i].x, cubes[i].y, cubes[i].size, cubes[i].size);
+        } else {
+            cubes_temp.push(cubes[i])
+        }
+    }
+    // update new cubes array
+    cubes = cubes_temp
+
+    // move all upper cubes one row below
+    update_pos(cubes, "del_row")
+
 }
 
 init();
